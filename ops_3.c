bd758bb278023a5aa96cb70dde0b4fdc97fbcab2
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-09 06:22:06 -0400

    betty corrections

--- ops_3.c
+++ ops_3.c
@@ -21,7 +21,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	num = (*stack)->n;
 
 	if (num < 0 || num > 127)
