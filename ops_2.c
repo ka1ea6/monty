8b76204c6f34e4208059876038810d25b291b7e0
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-09 05:06:39 -0400

    sub corrections

--- ops_2.c
+++ ops_2.c
@@ -49,12 +49,12 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	if (!(*stack))
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if (!(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	diff = 0;
