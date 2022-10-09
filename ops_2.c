a94215f72c59d5bb21cc8cf96cfa1c21ba6c9638
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-09 12:02:46 +0300

    sub

--- ops_2.c
+++ ops_2.c
@@ -33,3 +33,37 @@ void add(stack_t **stack, unsigned int line_number)
 	(*stack) = (*stack)->next;
 
 }
+
+/**
+ * sub - adds the top two elements of the stack
+ *
+ * @stack: pointer to the head of the stack
+ * @line_number: line number.
+ *
+ * Return: Nothing.
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	int diff;
+
+	if (!(*stack))
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (!(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	diff = 0;
+	diff += (*stack)->n;
+	diff += (*stack)->next->n;
+
+	(*stack)->next->n = sum;
+	(*stack)->next->prev = NULL;
+
+	(*stack) = (*stack)->next;
+
+}
