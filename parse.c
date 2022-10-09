49ff1038ad60ea90b12ca9f642e49eae0d656758
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-09 05:45:17 -0400

    mul correction

--- parse.c
+++ parse.c
@@ -78,7 +78,7 @@ void get_op(char *token1, int *line_number,  stack_t **head)
 		{"add", add_stack},
 		{"sub", sub_stack},
 		{"div", div_stack},
-		{"div", mul_stack},
+		{"mul", mul_stack},
 	};
 
 	fun_arr_length = (int) sizeof(fun_arr) / sizeof(fun_arr[0]);
